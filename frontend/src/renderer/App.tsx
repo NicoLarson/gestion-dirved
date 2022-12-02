23f2abc4132fccaa17ffdb2c1a25c48dff76c64b
NicoLarson <nicolas.yang.1@gmail.com>
2022-12-02 20:37:50 -0300

    First page

--- frontend/src/renderer/App.tsx
+++ frontend/src/renderer/App.tsx
@@ -3,40 +3,14 @@ import '../assets/css/reset.css';
 import '../assets/css/theme.css';
 import '../assets/css/style.css';
 import './App.css';
-import React, { useState, useEffect } from 'react';
-// import Conventions from '../models/conventions';
-import DisplayConventions from '../components/DisplayConventions/DisplayConventions';
 
-const AppGestion = () => {
-  const [title, setTitle] = useState<string>('Gestion DiRVED');
-  const [fetchedData, updateFetchedData] = useState([]);
-
-  const api = 'http://localhost:5000/conventions';
-  useEffect(() => {
-    fetch(api)
-      .then((response) => response.json())
-      .then((data) => updateFetchedData(data));
-  }, []);
-
-  return (
-    <div className="appGestion">
-      <header>
-        <h1>{title}</h1>
-      </header>
-      <main>
-        <h2>Il y a {fetchedData.length} convention</h2>
-        <DisplayConventions conventions={fetchedData} />
-      </main>
-      <footer>Info</footer>
-    </div>
-  );
-};
+import Home from '../pages/Home';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<AppGestion />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
